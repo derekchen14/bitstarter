a3eeada260878d753f384bd45b14d7ff0a7e8fa5
Derek Chen <derekchen14@gmail.com>
2013-07-08 18:37:38 -0700

    Modified web.js to read from index file

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var util = require('util');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var myBuffer = fs.readFileSync("index.html")
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(myBuffer.toString());
 });
 
 var port = process.env.PORT || 5000;
